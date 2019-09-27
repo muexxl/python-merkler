cfcc711b768f7c3abef59d3e016685c34c6e7b96
Muexx <stephan.muekusch@gmail.com>
2019-09-27 08:49:29 +0200

    added functionality: adding Files to merkle tree adding Folder to merkle tree adding Folders recursively to merkle tree getting merkle branches from filename introduced isUpdated flag to ensure exports are only done after updating

--- merkler/merkler.py
+++ merkler/merkler.py
@@ -1,17 +1,40 @@
 import hashlib
 import json
 import binascii
+import os
+
+def calcFileHash(filename):
+    sha256_hash = hashlib.sha256()
+    with open(filename,"rb") as f:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f.read(4096),b""):
+            sha256_hash.update(byte_block)
+    fileHash=sha256_hash.digest()
+    return fileHash
+
+def calcHash(data):
+    return hashlib.sha256(data).digest
+
+def isHash(data):
+    result = True
+    if not type(data) == bytes:
+        result= False
+    elif not len(data) == 32:
+        result= False
+    return result
 
 class Merkler(object):
 
     def __init__(self):
+        self.isUpdated = False
         self.merkle_tree = []
         hashes=[]
         self.merkle_tree.append(hashes)
 
     def addHash(self, data):
-        if not self.isHash(data):
+        if not isHash(data):
             raise Exception ("Merkler.add_hash was called with non hash data")
+        self.isUpdated = False
         self.merkle_tree[0].append(data)
 
 
@@ -22,6 +45,11 @@ class Merkler(object):
         while len(hashes) > 1: #check if highest level is reached
             hashes = self.CalculateNextMerkleTreeLevel(hashes)[:]
             self.merkle_tree.append(hashes)
+
+    def update(self):
+        if not self.isUpdated:
+            self.BuildMerkleTree()
+        self.isUpdated=True
 
     def CalculateNextMerkleTreeLevel(self, hashes):
         current_level_hashes = hashes[:]
@@ -39,16 +67,38 @@ class Merkler(object):
         return next_level_hashes
 
     def addFile(self, filename):
-        sha256_hash = hashlib.sha256()
-        with open(filename,"rb") as f:
-            # Read and update hash string value in blocks of 4K
-            for byte_block in iter(lambda: f.read(4096),b""):
-                sha256_hash.update(byte_block)
-        fileHash=sha256_hash.digest()
+        fileHash=calcFileHash(filename)
         self.addHash(fileHash)
         return binascii.hexlify(fileHash).decode('utf-8')
 
+    def addMultipleFiles(self, files):
+        if not isinstance(files, type([])):
+            print (type(files))
+            raise RuntimeError('passed object is not of type list')
+        for f in files:
+            self.addFile(f)
+
+    def addAllFilesInFolder(self, folder):
+        if not os.path.isdir(folder):
+            raise RuntimeError('{} is not a valid folder'.format(folder))
+            return
+        allFiles= []
+        for fld, subfolders, files in os.walk(folder):
+            allFiles.extend([ os.path.join(fld, f) for f in files])
+            break #only do the highest folder. Do not go into subfolders
+        self.addMultipleFiles(allFiles)
+
+    def addAllFilesInFolderRecursive(self, folder):
+        if not os.path.isdir(folder):
+            raise RuntimeError('{} is not a valid folder'.format(folder))
+            return
+        allFiles= []
+        for fld, subfolders, files in os.walk(folder):
+            allFiles.extend([ os.path.join(fld, f) for f in files])
+        self.addMultipleFiles(allFiles)
+
     def exportMerkleTreeAsString(self):
+        self.update()
         string_merkle_tree=[]
         for level in self.merkle_tree:
             string_level=[]
@@ -93,7 +143,8 @@ class Merkler(object):
 
         return result
 
-    def getMerkleBranch(self, target_hash):
+    def getMerkleBranchFromHash(self, target_hash):
+        self.update()
         try:
             position=self.merkle_tree[0].index(target_hash)
         except ValueError:
@@ -119,16 +170,16 @@ class Merkler(object):
         mb.merkleRootHash = self.merkle_tree[-1][0] # add merkle root
         return mb
 
-    def isHash(self, data):
+    def getMerkleBranchFromFile(self, filename):
 
-        result = True
+        if not os.path.isfile(filename):
+            raise RuntimeError('Expected a filename. {} is not a valid filename'.format(filename))
 
-        if not type(data) == bytes:
-            result= False
-        elif not len(data) == 32:
-            result= False
+        targetHash= calcFileHash(filename)
+        merkleBranch=self.getMerkleBranchFromHash(targetHash)
+        return merkleBranch
 
-        return result
+
 
     def calcHash(self, data):
         return hashlib.sha256(data).digest()
@@ -188,3 +239,32 @@ class MerkleBranch(object):
             jstring=f.read()
         self.loadFromJSON(jstring)
         return self.verify()
+
+
+def test():
+    files = []
+    raise ValueError ('unexpeted')
+    import sys
+    path=os.path.join(os.getcwd(), '')
+    wk=os.walk(path)
+    m=Merkler()
+    m.merkle_tree
+    m.addAllFilesInFolderRecursive(os.getcwd())
+    m.addMultipleFiles(['setup.py', 'LICENSE'])
+    m.BuildMerkleTree()
+    m.exportMerkleTreeAsString()
+    mb=m.getMerkleBranchFromFile('tests/merkler_tests.py')
+    mb.verify()
+    mb.exportAsJSON()
+    dir(w)
+    os.path.isdir('.git')
+    string='asdsdaf'
+    isinstance("string", str)
+    list=[1,2,4]
+    list2=[5,6,7]
+    list.extend(list2)
+    [list.append(l) for l in list2]
+    list.append(list2)
+    for dir, subfolders, files in wk:
+        [print(os.path.join(dir,f)) for f in files]
+        break
